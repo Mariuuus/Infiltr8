c56a03fe67a80492b8b5c1149b9cdd0c6dcdcb12
Mariuuus <63265366+Mariuuus@users.noreply.github.com>
2025-05-07 14:26:21 +0200

    add states and fix folder structure

---
+++ INFiltr8/Assets/!_ProjectMain/Scripts/Managers/States/LevelState.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace __ProjectMain.Scripts.Managers.States
+{
+    [System.Serializable]
+    public class LevelState
+    {
+        public Vector2Int leftUpperWall;
+        public Vector2Int rightBottomWall;
+        public Transform spawnPoint;
+        
+        public List<SubnetworkState> subnetworkStates;
+        
+        public Vector2Int flag;
+
+        public LevelState () {
+        }
+        
+    }
+}
---
+++ INFiltr8/Assets/!_ProjectMain/Scripts/Managers/States/SubnetworkState.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace __ProjectMain.Scripts.Managers.States
+{
+    [System.Serializable]
+    public class SubnetworkState
+    {
+        public Vector2Int leftUpperWall;
+        public Vector2Int rightBottomWall;
+        public Vector2Int leftUpperDoor;
+        public Vector2Int rightBottomDoor;
+        // Todo: What is in need to open the door. a list of enums and numbers.
+    }
+}
