dad5cafa6844e46de739ae0c5c517ee0201cf414
Mariuuus <63265366+Mariuuus@users.noreply.github.com>
2025-05-07 13:33:27 +0200

    add folder structure

---
+++ INFiltr8/Assets/!_ProjectMain/Scenes/Map/please_delete_on_first_file_insert.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class please_delete_on_first_file_insert : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
