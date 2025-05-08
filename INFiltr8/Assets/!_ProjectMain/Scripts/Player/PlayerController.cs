2406d1f3745a33486e2465c98dc10d1007befdba
Maxcfs2003 <80531975+Maxcfs2003@users.noreply.github.com>
2025-05-08 21:25:32 +0200

    player controller basics

---
+++ INFiltr8/Assets/!_ProjectMain/Scripts/Player/PlayerController.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private InputAction moveAction;
+    
+    [SerializeField]
+    private float Speed = 2;
+
+    [SerializeField] 
+    private Rigidbody rb;
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        
+        rb.MovePosition(transform.position + new Vector3(moveValue.x, 0, moveValue.y).normalized * Time.deltaTime * Speed);
+        
+        InputSystem.onEvent +=
+            (eventPtr, device) =>
+            {
+              //  Debug.Log($"Received event for {device}");
+            };
+    }
+}
