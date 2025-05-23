7eaad7090b9a20a05506abee61ecc2c86bd9b22e
Maxcfs2003 <80531975+Maxcfs2003@users.noreply.github.com>
2025-05-23 22:07:20 +0200

    change color of objects depending on hackColor

--- INFiltr8/Assets/!_ProjectMain/Scripts/Objects/GrabbableType.cs
+++ INFiltr8/Assets/!_ProjectMain/Scripts/Objects/GrabbableType.cs
@@ -6,9 +6,35 @@ public class grabbableType : MonoBehaviour
 {
     [SerializeField] private HackStatus hackColor;
 
+    [SerializeField] private Material redMat;
+    [SerializeField] private Material blueMat;
+    [SerializeField] private Material greenMat;
+    [SerializeField] private Material yellowMat;
+
     private void Start()
     {
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+        Material material = null;
+
+        switch (hackColor)
+        {
+            case HackStatus.BlueHacked:
+                material = blueMat;
+                break;
+            case HackStatus.RedHacked:
+                material = redMat;
+                break;
+            case HackStatus.GreenHacked:
+                material = greenMat;
+                break;
+            case HackStatus.YellowHacked:
+                material = yellowMat;
+                break;
+        }
         
+        Debug.Log(renderer == null);
+
+        renderer.material = material;
     }
     public HackStatus getHackColor()
     {
