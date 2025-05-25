32d2e5645a6b455ad7f01a502e10ab7d7c3376fa
Maxcfs2003 <80531975+Maxcfs2003@users.noreply.github.com>
2025-05-25 19:59:26 +0200

    added color change cooldown + ui

--- INFiltr8/Assets/!_ProjectMain/Scripts/Objects/GrabbableType.cs
+++ INFiltr8/Assets/!_ProjectMain/Scripts/Objects/GrabbableType.cs
@@ -1,29 +1,39 @@
 using System;
+using System.Collections;
 using __ProjectMain.Scripts.LevelEditor.Types;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 public class grabbableType : MonoBehaviour
 {
     [SerializeField] private HackStatus hackColor;
-
+    [SerializeField] private int cooldownTime;
+    
     [SerializeField] private Material redMat;
     [SerializeField] private Material blueMat;
     [SerializeField] private Material greenMat;
     [SerializeField] private Material yellowMat;
+    
+    [SerializeField] private Canvas countdownUI;
+    private Canvas _countDownUIInstance;
+    
+    private bool onCooldown = false;
 
     private void Start()
     {
-       changeMaterial(hackColor);
+       setMaterial(hackColor);
     }
     public HackStatus getHackColor()
     {
         return hackColor;
     }
     
-    public void changeMaterial(HackStatus Color) {
+    private void setMaterial(HackStatus Color) {
         MeshRenderer renderer = GetComponent<MeshRenderer>();
         Material material = null;
-    
+        
         switch (Color) {
             case HackStatus.BlueHacked:
                 material = blueMat;
@@ -41,4 +51,72 @@ public class grabbableType : MonoBehaviour
         
         renderer.material = material;
     }
+
+    public void changeMaterial(HackStatus Color)
+    {
+        if (onCooldown) return;
+
+        StartCoroutine(cooldown());
+        StartCoroutine(updateUIpos());
+        StartCoroutine(updateUIFill()); 
+        setMaterial(Color);
+    }
+
+    IEnumerator updateUIpos()
+    {
+        while (onCooldown)
+        {
+            if (_countDownUIInstance != null)
+            {
+                _countDownUIInstance.transform.position = transform.position + new Vector3(0, 3, 0);
+            }
+            else
+            {
+                yield break;
+            }
+            
+            yield return null;
+        }
+        
+    }
+
+    IEnumerator updateUIFill()
+    {
+        if (_countDownUIInstance != null)
+        {
+            float elapsed = 0f;
+            Image image = _countDownUIInstance.GetComponentInChildren<Image>();
+            
+            while (elapsed < cooldownTime && onCooldown)
+            {
+                elapsed += Time.deltaTime;
+                image.fillAmount = Mathf.Lerp(1f, 0f, elapsed / cooldownTime);
+                yield return null; 
+            }
+            
+            image.fillAmount = 0f;
+        }
+    }
+    
+     IEnumerator cooldown()
+    {   
+        onCooldown = true;
+        int remaining = cooldownTime;
+        // float decreaseStep = 1.0f / cooldownTime;
+        _countDownUIInstance = Instantiate(countdownUI, transform.position + new Vector3(0, 3, 0), Quaternion.identity);
+
+        while (remaining > 0)
+        {
+            yield return new WaitForSeconds (1);
+            //float currentFill = _countDownUIInstance.GetComponentInChildren<Image>().fillAmount;
+            //float smoothed = Mathf.Lerp(currentFill, currentFill - decreaseStep, 0.5f);
+            //_countDownUIInstance.GetComponentInChildren<Image>().fillAmount -= smoothed;
+            remaining--;
+        }
+        
+        Destroy(_countDownUIInstance.GameObject());
+        onCooldown = false;
+        yield return null;
+    } 
 }
+
