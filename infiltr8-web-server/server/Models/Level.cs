9114ab7ccb8333fff7e4879301aa62ff028cba03
Marius Strauss <marius.strauss@strauss-esolutions.de>
2025-08-21 08:44:00 +0200

    add webserver publish (pagination route and further information)

--- infiltr8-web-server/server/Models/Level.cs
+++ infiltr8-web-server/server/Models/Level.cs
@@ -1,10 +1,15 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System;
 
 public class Level
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
-    public string Content { get; set; }
+    public string Id { get; set; } = string.Empty;
+
+    public required string Name { get; set; }
+    public required string Author { get; set; }
+    public DateTime UploadDate { get; set; }
+    public required string Content { get; set; }
 }
