3cd441ad7642bc0e115c4fd42cf8cfc33dc70db6
Maxcfs2003 <80531975+Maxcfs2003@users.noreply.github.com>
2025-08-24 23:02:40 +0200

    add vertical laser wall movement, add cooldown when touched and laser wall materials

--- INFiltr8/Assets/!_ProjectMain/Scripts/Objects/LaserWallController.cs
+++ INFiltr8/Assets/!_ProjectMain/Scripts/Objects/LaserWallController.cs
@@ -1,41 +1,73 @@
 using System;
+using System.Collections;
+using System.Numerics;
 using __ProjectMain.Scripts.Player;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 namespace __ProjectMain.Scripts.Objects
 {
     public class LaserWallController : MonoBehaviour
     {
-        private Vector3 initialPosition;
-        private Vector3 direction = Vector3.left;
-        public float speed = 2f;
-        public float maxMoveDistance = 5f;
+        [SerializeField] private float speed = 2f;
+        [SerializeField] private float maxMoveDistance = 5f;
+        [SerializeField] private int cooldownTime = 3;
+        [SerializeField] private bool isVertical = false;
+        [SerializeField] private Material normalMaterial;
+        [SerializeField] private Material transparentMaterial;
+        
+        private Vector3 _initialPosition;
+        private Vector3 _direction = Vector3.right;
+        private bool _onCooldown = false;
+        private MeshRenderer _meshRenderer;
   
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-            this.initialPosition = transform.position;
+            this._initialPosition = transform.position;
+            this._meshRenderer = gameObject.GetComponent<MeshRenderer>();
+            this._meshRenderer.material = normalMaterial;
+            if (isVertical)
+            {
+                transform.Rotate(0f, 90f, 0f);
+            }
         }
 
         // Update is called once per frame
         void FixedUpdate()
         {
-            transform.Translate(direction * speed * Time.deltaTime);
+            transform.Translate(_direction * speed * Time.deltaTime);
 
-            if (transform.position.x <= (this.initialPosition.x - this.maxMoveDistance))
+            if (!isVertical)
             {
-                this.direction = Vector3.right;
-            } else if (transform.position.x >= (this.initialPosition.x + this.maxMoveDistance))
+                if (transform.position.x <= (this._initialPosition.x - this.maxMoveDistance))
+                {
+                    this._direction = Vector3.right;
+                } else if (transform.position.x >= (this._initialPosition.x + this.maxMoveDistance))
+                {
+                    this._direction = Vector3.left;
+                }     
+            } else if (isVertical)
             {
-                this.direction = Vector3.left;
+                if (transform.position.z <= (this._initialPosition.z - this.maxMoveDistance))
+                {
+                    this._direction = Vector3.left;
+                } else if (transform.position.z >= (this._initialPosition.z + this.maxMoveDistance))
+                {
+                    this._direction = Vector3.right;
+                }     
             }
+           
         }
 
         private void OnTriggerEnter(Collider other)
         {
+            if (_onCooldown) return;
+            
             if (other.CompareTag("Player"))
             {
                 GrabController grabController = other.GetComponent<GrabController>();
+                StartCoroutine(Cooldown());
 
                 if (grabController.IsGrabbing)
                 {
@@ -45,6 +77,23 @@ namespace __ProjectMain.Scripts.Objects
           
                 Debug.Log("player hit laser wall!");
             }
+        }
+
+        IEnumerator Cooldown()
+        {
+            _onCooldown = true;
+            int remainingTime = cooldownTime;
+            this._meshRenderer.material = transparentMaterial;
+            
+            while (remainingTime > 0)
+            {
+                yield return new WaitForSeconds(1);
+                remainingTime--;
+            }
+
+            this._meshRenderer.material = normalMaterial;
+            _onCooldown = false;
+            yield return null;
         }
     }
 }
