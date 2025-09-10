5c3f0a997b702d1215e4f1c4591f5dd86562fdbd
Maxcfs2003 <80531975+Maxcfs2003@users.noreply.github.com>
2025-09-10 21:57:57 +0200

    added camera functionality to cameraController and fov (TODO: add security cams to leveleditor)

--- INFiltr8/Assets/!_ProjectMain/Scripts/Objects/CameraController.cs
+++ INFiltr8/Assets/!_ProjectMain/Scripts/Objects/CameraController.cs
@@ -1,32 +1,122 @@
+using System;
+using System.Collections;
 using Unity.Mathematics;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
       
-    [SerializeField] private float range = 500.0f;
+    [SerializeField] private float maxDistance = 5.0f;
+    [SerializeField] private GameObject FOV;
+    [SerializeField] private int rotationCooldownTime = 2;
+    public float rotationSpeed = 3.0f;
+    public bool rotateRight = true;
+    private int mask;
 
-    [SerializeField] private float rotationSpeed = 3.0f;
+    private bool rotationCoolDown = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Init();
+    }
+
+    public void Init()
+    {
+       // StartCoroutine(rotateCamera());
+        mask = LayerMask.GetMask("Wall");
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-          Vector3 offset = new Vector3(+0.15f, 0.30f, 0f);
-          RaycastHit hit;
-          if (Physics.Raycast(transform.position + offset, transform.TransformDirection(Vector3.back), out hit, math.INFINITY))
-          { 
-                Debug.DrawRay(transform.position + offset, transform.TransformDirection(Vector3.back) * math.INFINITY, Color.yellow); 
-                Debug.Log("Did Hit"); 
-          }
-          else
-          { 
-                Debug.DrawRay(transform.position + offset,transform.TransformDirection(Vector3.back) * math.INFINITY, Color.white); 
-                Debug.Log("Did not Hit"); 
-          }
+        Vector3 origin = transform.position + new Vector3(0f, 0.30f, 0f);
+        Vector3 direction = transform.TransformDirection(Vector3.forward);
+        Vector3 directionLeft = Quaternion.AngleAxis(-30f, Vector3.up) * transform.forward;
+        Vector3 directionRight = Quaternion.AngleAxis(30f, Vector3.up) * transform.forward;
+
+        //RaycastHit hit;
+        RaycastHit hitLeft;
+        RaycastHit hitRight;
+        RaycastHit hitMiddle;
+        RaycastHit hitRightMiddle;
+        RaycastHit HitLeftMiddle;
+        
+        drawRayCast(origin, direction, out hitMiddle, maxDistance / 1.35f, mask, true);
+        //drawRayCast(origin, direction, out hit, maxDistance, mask, false);
+        drawRayCast(origin, directionLeft, out hitLeft, maxDistance * 1.15f, mask, false);
+        drawRayCast(origin, directionRight, out hitRight, maxDistance * 1.15f, mask, false);
+        drawRayCast(origin, directionLeft, out HitLeftMiddle, maxDistance * 0.9f, mask, true);
+        drawRayCast(origin, directionRight, out hitRightMiddle, maxDistance * 0.9f, mask, true);
+
+        
+        float rotateDirection = rotateRight ? 1f : -1f;
+        transform.Rotate(0f, rotateDirection * rotationSpeed * Time.deltaTime, 0f);
+    }
+
+    private void drawRayCast(Vector3 origin, Vector3 direction, out RaycastHit hit, float maxDistance, LayerMask layer, bool isDisabling)
+    {
+        if (Physics.Raycast(origin, direction, out hit, maxDistance, layer))
+        {
+            if (isDisabling)
+            {
+                Debug.DrawRay(origin, direction * hit.distance, Color.red);     
+            }
+            else
+            {
+                Debug.DrawRay(origin, direction * hit.distance, Color.yellow);  
+            }
+            changeRotationDirection();
+            if (isDisabling)
+            {
+               FOV.GetComponent<CameraFOVController>().EnableInactiveMode();
+            }
+        }
+        else
+        {
+            Debug.DrawRay(origin, direction * maxDistance, Color.white);
+            if (isDisabling)
+            {
+                FOV.GetComponent<CameraFOVController>().DisableInactiveMode();
+            }
+        }
+    }
+
+    IEnumerator rotateCamera()
+    {
+        Vector3 startRot = transform.eulerAngles;
+
+        while(true)
+        {
+            float t = 0f;
+            while (t < 1f)
+            {
+                t += Time.deltaTime / rotationSpeed;
+                float angle = Mathf.Lerp(0f, 360f, t);
+                transform.eulerAngles = startRot + new Vector3(0f, rotateRight ? angle : -angle, 0f);
+                yield return null;
+            }
+        }
+    }
+
+    private void changeRotationDirection()
+    {
+        if (rotationCoolDown) return;
+        this.rotateRight = !rotateRight;
+        StartCoroutine(rotationCooldownCoroutine());
+    }
+    
+    IEnumerator rotationCooldownCoroutine()
+    {
+        rotationCoolDown = true;
+        int remainingTime = rotationCooldownTime;
+        while (remainingTime > 0)
+        {
+               
+            yield return new WaitForSeconds(1);
+            remainingTime--;
+        }
+
+        rotationCoolDown = false;
+        yield return null;
     }
 }
---
+++ INFiltr8/Assets/!_ProjectMain/Scripts/Objects/CameraFOVController.cs
@@ -0,0 +1,41 @@
+using System;
+using __ProjectMain.Scripts.Managers.TimeTracker;
+using NUnit.Framework.Constraints;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraFOVController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private bool isInactive = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isInactive) return;
+        Debug.Log("caught player");
+        LevelTimeTracker.Instance?.SetMultiplier(2f);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (isInactive) return;
+        Debug.Log("player got out of sight");
+        LevelTimeTracker.Instance?.SetMultiplier(1f);
+    }
+
+    public void EnableInactiveMode()
+    {
+        isInactive = true;
+        var renderer = GetComponent<Renderer>();
+        Material mat = renderer.material;
+        mat.SetColor("_Color", new Color(0.74901f, 0.15686f, 0.00784f, 1f) * 2f);
+    }
+
+    public void DisableInactiveMode()
+    {
+        isInactive = false;
+        var renderer = GetComponent<Renderer>();
+        Material mat = renderer.material;
+        mat.SetColor("_Color", new Color(0.74901f, 0.15686f, 0.00784f, 1f) * 32f);
+    }
+}
